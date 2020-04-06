cd1da2926470ac2fdbe9261a4a4a1fb5da499228
ANTONY JUDE PRASAN POOBALARAYAN <prasan29@yahoo.com>
2020-04-06 01:32:02 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,4 +1,10 @@
-
+katz_deli = []
+def line(katz_deli)
+   if katz_deli.length > 0
+      katz_deli.map.with_index {|name, index| puts "The line is currently:#{index + 1}. #{name}"}
+   else
+     puts "The line is currently empty."
+   end
 
 
 
